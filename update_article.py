098cb69c63ec5a27f0ebd1da20374c760ab25e71
JoshuaStapleton21 <jstapleton17@apu.edu>
2021-07-24 17:13:23 +0200

    updated mozart article

--- update_article.py
+++ update_article.py
@@ -3,7 +3,7 @@ from publish import publish_an_article
 # essentially just updates the html page itself. No changes to app.py or index.html
 
 ########### THIS IS THE ONLY PARAMETER WHICH NEEDS TO BE CONFIGURED IN THE WHOLE FILE ##########
-article_name = "ai_horror"
+article_name = "feral_mozart"
 ################################################################################################
 
 args_in = ['/Users/joshuastapleton/Desktop/joshua_stapleton_blog/blogmaker/publish.py', 'posts/'+ str(article_name) + '.md']
